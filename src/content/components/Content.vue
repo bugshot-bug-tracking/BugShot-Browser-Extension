a27426633bc54577cf51b4bcf061e57e19f7f487
Radu Memetea <r.memetea99@proxima-idea.com>
2021-09-29 14:40:04 +0300

    Small changes

--- src/content/components/Content.vue
+++ src/content/components/Content.vue
@@ -1,113 +1,113 @@
 <template>
-    <div id="bugshot-container">
-        <Sidebar
-            v-show="showSidebar"
-            @default="setDefault"
-            @add="addEvent"
-            @list="listEvent"
-        />
+	<div id="bugshot-container">
+		<Sidebar
+			v-show="showSidebar"
+			@default="setDefault"
+			@add="addEvent"
+			@list="listEvent"
+		/>
 
-        <ListTab v-if="showList" @info="infoEvent" />
+		<Buglist v-if="showList" @info="infoEvent" />
 
-        <InfoTab v-if="showInfo" :bug="bugInfo" @close="closeInfo" />
+		<InfoTab v-if="showInfo" :bug="bugInfo" @close="closeInfo" />
 
-        <FormTab v-if="showForm" :bug="bugDetails" @default="setDefault" />
+		<FormTab v-if="showForm" :bug="bugDetails" @default="setDefault" />
 
-        <Overlay
-            v-if="showOverlay"
-            :details="bugDetails"
-            @sidebar="showSidebar = true"
-            @default="setDefault"
-            @formOpen="formOpen"
-        />
-    </div>
+		<Overlay
+			v-if="showOverlay"
+			:details="bugDetails"
+			@sidebar="showSidebar = true"
+			@default="setDefault"
+			@formOpen="formOpen"
+		/>
+	</div>
 </template>
 
 <script>
 import { nextTick, ref } from "vue";
 
 import Sidebar from "./sidebar/Sidebar.vue";
-import ListTab from "./listTab/Buglist.vue";
+import Buglist from "./listTab/Buglist.vue";
 import InfoTab from "./infoTab/InfoTab.vue";
 import FormTab from "./formTab/FormTab.vue";
 import Overlay from "./overlay/Overlay.vue";
 
 export default {
-    components: { Sidebar, ListTab, InfoTab, FormTab, Overlay },
-    setup() {
-        const showSidebar = ref(true);
-        const showList = ref(false);
-        const showInfo = ref(false);
-        const showForm = ref(false);
-        const showOverlay = ref(false);
+	components: { Sidebar, Buglist, InfoTab, FormTab, Overlay },
+	setup() {
+		const showSidebar = ref(true);
+		const showList = ref(false);
+		const showInfo = ref(false);
+		const showForm = ref(false);
+		const showOverlay = ref(false);
 
-        const bugInfo = ref({});
+		const bugInfo = ref({});
 
-        const bugDetails = ref({});
+		const bugDetails = ref({});
 
-        const setDefault = () => {
-            showSidebar.value = true;
-            showList.value = false;
-            showInfo.value = false;
-            showForm.value = false;
-            showOverlay.value = false;
+		const setDefault = () => {
+			showSidebar.value = true;
+			showList.value = false;
+			showInfo.value = false;
+			showForm.value = false;
+			showOverlay.value = false;
 
-            bugInfo.value = {};
-            bugDetails.value = {};
-        };
+			bugInfo.value = {};
+			bugDetails.value = {};
+		};
 
-        const addEvent = () => {
-            setDefault();
+		const addEvent = () => {
+			setDefault();
 
-            // wait for the values to change to default then execute this
-            nextTick(() => {
-                showSidebar.value = false;
-                showOverlay.value = true;
-            }, 1);
-        };
+			// wait for the values to change to default then execute this
+			nextTick(() => {
+				showSidebar.value = false;
+				showOverlay.value = true;
+			}, 1);
+		};
 
-        const listEvent = () => {
-            showList.value = !showList.value;
-            if (showList.value === false) showInfo.value = false;
-        };
+		const listEvent = () => {
+			showList.value = !showList.value;
+			if (showList.value === false) showInfo.value = false;
+		};
 
-        const infoEvent = (bug) => {
-            bugInfo.value = bug;
-            if (showInfo.value !== true) showInfo.value = !showInfo.value;
-            // console.info({ info: bug });
-        };
+		const infoEvent = (bug) => {
+			bugInfo.value = bug;
+			if (showInfo.value !== true) showInfo.value = !showInfo.value;
+			// console.info({ info: bug });
+		};
 
-        const closeInfo = () => {
-            showInfo.value = false;
-        };
+		const closeInfo = () => {
+			showInfo.value = false;
+		};
 
-        const formOpen = (details) => {
-            bugDetails.value.resolution = {
-                width: screen.width * window.devicePixelRatio,
-                height: screen.height * window.devicePixelRatio,
-            };
+		const formOpen = (details) => {
+			bugDetails.value.resolution = {
+				width: screen.width * window.devicePixelRatio,
+				height: screen.height * window.devicePixelRatio,
+			};
 
-            console.log(bugDetails.value);
+			console.log(bugDetails.value);
 
-            showForm.value = true;
-        };
+			showForm.value = true;
+		};
 
-        return {
-            showSidebar,
-            showList,
-            showInfo,
-            showForm,
-            bugInfo,
-            showOverlay,
-            bugDetails,
-            setDefault,
-            addEvent,
-            listEvent,
-            infoEvent,
-            closeInfo,
-            formOpen,
-        };
-    },
+		return {
+			showSidebar,
+			showList,
+			showInfo,
+			showForm,
+			bugInfo,
+			showOverlay,
+			bugDetails,
+			setDefault,
+			addEvent,
+			listEvent,
+			infoEvent,
+			closeInfo,
+			formOpen,
+		};
+	},
 };
 </script>
 
--- src/content/components/overlay/Overlay.vue
+++ src/content/components/overlay/Overlay.vue
@@ -1,154 +1,159 @@
 <template>
-    <div
-        id="overlay"
-        :class="{ marked: mark.on }"
-        v-show="showOverlay"
-        @click.once="marked"
-    >
-        <div
-            class="marker"
-            v-if="mark.on"
-            :style="`left: ${mark.x - 16}px; top: ${mark.y - 56}px;`"
-            :class="priority"
-        ></div>
-    </div>
+	<div
+		id="overlay"
+		:class="{ marked: mark.on }"
+		v-show="showOverlay"
+		@click.once="marked"
+	>
+		<div
+			class="marker"
+			v-if="mark.on"
+			:style="`left: ${mark.x - 16}px; top: ${mark.y - 56}px;`"
+			:class="priority"
+		></div>
+	</div>
 </template>
 
 <script>
 import { ref, computed } from "vue";
 
 export default {
-    name: "Overlay",
-    props: ["details", "default", "sidebar"],
-    setup(props, context) {
-        const lock = ref(false);
-        const showOverlay = ref(true);
+	name: "Overlay",
+	props: ["details", "default", "sidebar"],
+	setup(props, context) {
+		const lock = ref(false);
+		const showOverlay = ref(true);
 
-        const mark = ref({
-            on: false,
-            x: 0,
-            y: 0,
-        });
+		const mark = ref({
+			on: false,
+			x: 0,
+			y: 0,
+		});
 
-        const marked = (event) => {
-            event.stopImmediatePropagation();
+		const marked = (event) => {
+			event.stopImmediatePropagation();
 
-            // Prevent more than 1 click event while registering a bug report
-            if (lock.value) return;
-            lock.value = true;
+			// Prevent more than 1 click event while registering a bug report
+			if (lock.value) return;
+			lock.value = true;
 
-            // hide overlay for screenshot
-            showOverlay.value = false;
+			// hide overlay for screenshot
+			showOverlay.value = false;
 
-            setTimeout(() => {
-                // Take screenshot
-                chrome.runtime.sendMessage(
-                    {
-                        message: "takeScreenshot",
-                    },
-                    (response) => {
-                        const generateQuerySelector = (el) => {
-                            if (el.tagName.toLowerCase() == "html")
-                                return "html";
-                            let str = el.tagName.toLowerCase();
+			setTimeout(() => {
+				// Take screenshot
+				chrome.runtime.sendMessage(
+					{
+						message: "takeScreenshot",
+					},
+					(response) => {
+						const generateQuerySelector = (el) => {
+							if (el.tagName.toLowerCase() == "html")
+								return "html";
+							let str = el.tagName.toLowerCase();
 
-                            str += el.id != "" ? "#" + el.id : "";
+							str += el.id != "" ? "#" + el.id : "";
 
-                            if (
-                                (typeof el.className === "string" ||
-                                    el.className instanceof String) &&
-                                el.className !== ""
-                            ) {
-                                let classes = el.className.split(/\s/);
-                                for (let i = 0; i < classes.length; i++)
-                                    str += "." + classes[i];
-                            }
-                            return (
-                                generateQuerySelector(el.parentNode) +
-                                " > " +
-                                str
-                            );
-                        };
+							if (
+								(typeof el.className === "string" ||
+									el.className instanceof String) &&
+								el.className !== ""
+							) {
+								let classes = el.className.split(/\s/);
+								for (let i = 0; i < classes.length; i++)
+									str += "." + classes[i];
+							}
+							return (
+								generateQuerySelector(el.parentNode) +
+								" > " +
+								str
+							);
+						};
 
-                        props.details.selector = generateQuerySelector(
-                            document.elementFromPoint(
-                                event.clientX,
-                                event.clientY
-                            )
-                        );
+						props.details.selector = generateQuerySelector(
+							document.elementFromPoint(
+								event.clientX,
+								event.clientY
+							)
+						);
 
-                        // Show overlay again
-                        showOverlay.value = true;
+						// Show overlay again
+						showOverlay.value = true;
 
-                        if (response.message === "error") {
-                            context.emit("default");
-                            console.error(response.error);
-                            return;
-                        }
+						if (response.message === "error") {
+							context.emit("default");
+							console.error(response.error);
+							return;
+						}
 
-                        if (response.message !== "ok") {
-                            context.emit("default");
-                            console.error(
-                                "Something went wrong with the screenshot!"
-                            );
-                            return;
-                        }
+						if (response.message !== "ok") {
+							context.emit("default");
+							console.error(
+								"Something went wrong with the screenshot!"
+							);
+							return;
+						}
 
-                        console.log("Screenshot taken!");
+						console.log("Screenshot taken!");
 
-                        // Save screenshot in memory
-                        props.details.screenshot = response.payload;
-                        props.details.priority = 2;
+						// Save screenshot in memory
+						props.details.screenshot = response.payload;
+						props.details.priority = 2;
 
-                        // Show sidebar again
-                        context.emit("sidebar");
+						// Show sidebar again
+						context.emit("sidebar");
 
-                        // coordinates relative to what user sees on screen
-                        let clientX = event.clientX;
-                        let clientY = event.clientY;
+						// coordinates relative to what user sees on screen
+						let clientX = event.clientX;
+						let clientY = event.clientY;
 
-                        // coordinates relative to the top of the page(it considers scroll)
-                        // ? this will be usefull when adding a mark of bug later and well need absolute coordonates to page position
-                        let pageX = event.pageX;
-                        let pageY = event.pageY;
+						// coordinates relative to the top of the page(it considers scroll)
+						// ? this will be usefull when adding a mark of bug later and well need absolute coordonates to page position
+						let pageX = event.pageX;
+						let pageY = event.pageY;
 
-                        mark.value = {
-                            on: true,
-                            x: clientX,
-                            y: clientY,
-                        };
+						mark.value = {
+							on: true,
+							x: clientX,
+							y: clientY,
+						};
 
-                        // NEXT oppen bug form
-                        props.details.mark_coords = { x: clientX, y: clientY };
+						// NEXT open bug form
+						props.details.mark_coords = {
+							x: clientX,
+							y: clientY,
+							wx: pageX,
+							wy: pageY,
+						};
 
-                        context.emit("formOpen");
-                    }
-                );
-            }, 100);
-        };
+						context.emit("formOpen");
+					}
+				);
+			}, 100);
+		};
 
-        const priority = computed(() => {
-            switch (Number(props.details.priority)) {
-                case 1:
-                    return "minor";
-                case 2:
-                    return "normal";
-                case 3:
-                    return "important";
-                case 4:
-                    return "critical";
+		const priority = computed(() => {
+			switch (Number(props.details.priority)) {
+				case 1:
+					return "minor";
+				case 2:
+					return "normal";
+				case 3:
+					return "important";
+				case 4:
+					return "critical";
 
-                default:
-                    return "normal";
-            }
-        });
+				default:
+					return "normal";
+			}
+		});
 
-        return {
-            showOverlay,
-            mark,
-            priority,
-            marked,
-        };
-    },
+		return {
+			showOverlay,
+			mark,
+			priority,
+			marked,
+		};
+	},
 };
 </script>
