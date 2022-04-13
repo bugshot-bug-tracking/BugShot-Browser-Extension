6013344cad7e92a038344d7a7ef8904e64a4bb07
Radu Memetea <r.memetea@proxima-idea.com>
2022-04-13 15:18:43 +0300

    chore: :bookmark: 1.0.1 manifest

--- dist/popup/popup.js
+++ dist/popup/popup.js
@@ -1,5 +1,5 @@
 "use strict";
-(self["webpackChunk"] = self["webpackChunk"] || []).push([["/dist/popup/popup"],{
+(self["webpackChunkbug_shot_extension"] = self["webpackChunkbug_shot_extension"] || []).push([["/dist/popup/popup"],{
 
 /***/ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-5.use[0]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./src/content/components/global/container/Container.vue?vue&type=script&lang=js":
 /*!*******************************************************************************************************************************************************************************************************************!*\
