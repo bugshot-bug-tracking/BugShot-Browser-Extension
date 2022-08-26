00f2680b87e5c3ee9303cc1ee0de03fa586254b3
r.memetea <r.memetea@proxima-idea.com>
2022-08-26 16:19:25 +0300

    feat: :tada: base initialization for content script

---
+++ src/content/App.vue
@@ -0,0 +1,18 @@
+<template>
+	<div flex flex-row-reverse>
+	</div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss">
+@import "~/styles/global.scss";
+@import "~/styles/Style.scss";
+
+:host {
+	position: relative;
+	font-size: 16px;
+	color: #1a2040;
+}
+</style>
--- src/content/index.ts
+++ src/content/index.ts
@@ -1,1 +1,65 @@
-console.log("Content!");
+import "@webcomponents/webcomponentsjs/bundles/webcomponents-ce.js";
+import "uno.css";
+import { createApp } from "vue";
+
+import axi from "~/modules/axios";
+import pinia from "~/modules/pinia";
+
+import App from "./App.vue";
+
+// Firefox `browser.tabs.executeScript()` requires scripts return a primitive value
+(() => {
+	class BugShot extends HTMLElement {
+		constructor() {
+			super();
+			this.attachShadow({ mode: "open" });
+
+			let app = createApp(App);
+
+			pinia.install({ app });
+			axi.install({ app });
+
+			app.mount(this.shadowRoot);
+
+			const styleEl = document.createElement("link");
+
+			styleEl.setAttribute("rel", "stylesheet");
+			styleEl.setAttribute(
+				"href",
+				browser.runtime.getURL("content/style.css")
+			);
+			this.shadowRoot?.prepend(styleEl);
+		}
+	}
+
+	// check to see if the element was defined beforehand if not define it
+	if (!customElements.get("bugshot-sidebar"))
+		customElements.define("bugshot-sidebar", BugShot);
+
+	let bugshot = null;
+	// check to see if the page contains an instance of bugshot
+	let domBugshot = document.getElementsByTagName("bugshot-sidebar");
+
+	// if no instance found define add one otherwise get refference to the one present
+	if (domBugshot.length === 0) {
+		// create a containing element to host the custom element
+		bugshot = document.createElement("div");
+
+		bugshot.style.cssText = `
+		position: fixed;
+		bottom: 0;
+		right: 0;
+		width: fit-content;
+		height: fit-content;
+		overflow: hidden;
+		background-color: transparent;
+		z-index: 2047483647;
+		`;
+
+		bugshot.appendChild(document.createElement("bugshot-sidebar"));
+
+		document.body.append(bugshot);
+	} else {
+		bugshot = domBugshot[0];
+	}
+})();
