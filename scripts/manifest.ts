c8252d7f55b7a1963a082ba63c0785bf28500dcf
r.memetea <r.memetea@proxima-idea.com>
2022-10-10 13:00:13 +0300

    build: :construction_worker: add build config and scripts for 4 target browsers

--- scripts/manifest.ts
+++ scripts/manifest.ts
@@ -1,12 +1,21 @@
 import fs from "fs-extra";
-import { getManifest } from "~/manifest";
-import { log, r } from "./utils";
+import { getManifest, getManifestV2, Mode, Target } from "~/manifest";
+import { isDev, log, r, target } from "./utils";
+
+const mode = isDev ? Mode.DEV : Mode.PROD;
 
 export async function writeManifest() {
-	await fs.writeJSON(r("extension/manifest.json"), getManifest(), {
-		spaces: 4,
-	});
-	log("PRE", "write manifest.json");
+	await fs.writeJSON(
+		r(`dist/${mode}/${target}/manifest.json`),
+		target === Target.FIREFOX
+			? getManifestV2(target as Target)
+			: getManifest(target as Target),
+		{
+			spaces: 4,
+		}
+	);
+
+	log("INFO", `write manifest.json for ${target}`);
 }
 
 writeManifest();
--- vite.config.content.ts
+++ vite.config.content.ts
@@ -1,36 +1,38 @@
 import { defineConfig } from "vite";
-import { sharedConfig } from "./vite.config";
-import { r } from "./scripts/utils";
 import packageJson from "./package.json";
+import { isDev, r, target } from "./scripts/utils";
+import { sharedConfig } from "./vite.config";
 
 // bundling the content script using Vite
-export default defineConfig({
-	...sharedConfig,
+export default defineConfig(({ mode }) => {
+	return {
+		...sharedConfig,
 
-	define: {
-		"process.env.NODE_ENV": "'development'",
-	},
-
-	build: {
-		watch: {},
-		outDir: r("extension/dev/content"),
-		cssCodeSplit: false,
-		emptyOutDir: false,
-		sourcemap: "inline",
-		lib: {
-			entry: r("src/content/index.ts"),
-			name: packageJson.name,
-			formats: ["iife"],
+		define: {
+			"process.env.NODE_ENV": mode,
 		},
-		rollupOptions: {
-			output: {
-				entryFileNames: "[name].js",
-				extend: true,
-				format: "iife",
+
+		build: {
+			watch: isDev ? {} : undefined,
+			outDir: r(`dist/${mode}/${target}/content`),
+			cssCodeSplit: false,
+			emptyOutDir: false,
+			sourcemap: isDev ? "inline" : false,
+			lib: {
+				entry: r("src/content/index.ts"),
+				name: packageJson.name,
+				formats: ["iife"],
 			},
+			rollupOptions: {
+				output: {
+					entryFileNames: "[name].js",
+					extend: true,
+					format: "iife",
+				},
+			},
+			minify: !isDev,
 		},
-		minify: false,
-	},
 
-	plugins: [...sharedConfig.plugins!],
+		plugins: [...sharedConfig.plugins!],
+	};
 });
